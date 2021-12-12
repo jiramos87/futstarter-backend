45f67ed1e4906685fb40834c26c1dad65c40dffd
= <jiramos87@gmail.com>
2021-12-12 20:49:37 -0300

    agora sim9

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from views import *
 
 
 app = Flask(__name__, static_folder='static')
