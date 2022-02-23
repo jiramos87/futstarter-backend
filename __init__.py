4a412221b7405c98000483425b55c8a95cc1e7fe
jiramos87 <jiramos87@gmail.com>
2022-02-23 10:53:17 -0300

    heroku changes3

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 import os
 
-
+ENV = os.environ
 # import requests
 # from flask import request, abort, send_from_directory
 # from models import update, register, login, delete_user, make_admin, calculate_meta, get_sts, get_cfs, get_lws, get_rws, get_cams, get_cms, get_cdms, get_lbs, get_rbs, get_cbs, get_gks, get_squad_by_league
@@ -17,7 +17,11 @@ import os
 app = Flask(__name__, static_folder=os.path.abspath("/static"), static_url_path='')
 app.debug = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or "postgresql://javier:ibanez570@localhost:5432/futstarter"
+if ENV == 'dev':
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://javier:ibanez570@localhost:5432/futstarter"
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://namyczudfpxnaf:2d840075e00fc6846392af812a87736d9a9ef90d110c0d45567b358494a2a535@ec2-3-92-119-83.compute-1.amazonaws.com:5432/d3d1mnvm26jdpl"
+ 
     
 # app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://namyczudfpxnaf:2d840075e00fc6846392af812a87736d9a9ef90d110c0d45567b358494a2a535@ec2-3-92-119-83.compute-1.amazonaws.com:5432/d3d1mnvm26jdpl"
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///futstarter.db'
